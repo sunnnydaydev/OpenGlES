f7c17d5818634a0733dd8b917e0648f6751e33c2
SunnyDay <1246771571@qq.com>
2022-03-25 16:22:16 +0800

    update 入门篇总结

--- app/src/main/java/com/sunnyday/constraintlayout/opengles/render/OpenGlRender.kt
+++ app/src/main/java/com/sunnyday/constraintlayout/opengles/render/OpenGlRender.kt
@@ -1,6 +1,6 @@
 package com.sunnyday.constraintlayout.opengles.render
 
-import android.opengl.GLES20.GL_COLOR_BUFFER_BIT
+import android.opengl.GLES20.*
 import android.opengl.GLSurfaceView
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
@@ -9,15 +9,37 @@ import javax.microedition.khronos.opengles.GL10
  * Create by SunnyDay on 20:26 2021/12/26
  */
 class OpenGlRender : GLSurfaceView.Renderer {
+    /**
+     * 当绘制一帧时这个方法会被GLSurfaceView调用。
+     * ps：在这个方法中一定要绘制些东西，即使是一个清空屏幕的操作，
+     * 因为这个方法返回后，渲染缓冲区会被交换并显示到屏幕上，如果
+     * 什么都没有画，可能会看到屏幕闪烁效果。
+     * */
     override fun onDrawFrame(gl: GL10?) {
-       gl?.glClear(GL_COLOR_BUFFER_BIT)
+        // 清空屏幕所有颜色，
+       glClear(GL_COLOR_BUFFER_BIT)
     }
 
+    /**
+     * Surface被创建后，每次Surface尺寸变化时GLSurfaceView会调用这个方法。
+     * 如：横竖屏切换时Surface尺寸就会变化。
+     * */
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
-       gl?.glViewport(0,0,width, height)
+        /**
+         * 设置视口（Viewport）尺寸，也即告诉OpenGL渲染view的大小。
+         * */
+       glViewport(0,0,width, height)
     }
-
+    /**
+     * 当Surface被创建时候，GLSurfaceView会调用这个方法。
+     * ps：应用第一次运行时、当设备被唤醒或者用户从其他activity切回来时
+     * 方法也可能会被调用。
+     * */
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-        gl?.glClearColor(1f, 0f, 0f, 0f)
+        /**
+         * 设置清空屏幕用的颜色，参数：red、green、blue、alpha
+         * [0,1]代表颜色强度。如下屏幕清空会显示红色。
+         * */
+        glClearColor(1f, 0f, 0f, 0f)
     }
 }
--- app/src/main/java/com/sunnyday/constraintlayout/opengles/ui/MainActivity.kt
+++ app/src/main/java/com/sunnyday/constraintlayout/opengles/ui/MainActivity.kt
@@ -10,17 +10,28 @@ import com.sunnyday.constraintlayout.opengles.render.OpenGlRender
 
 class MainActivity : AppCompatActivity() {
     private lateinit var glSurfaceView: GLSurfaceView
+    /**
+     * 标记是否已经启用渲染器。方便处理生命周期问题。
+     * */
     private var rendererSet = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //1、创建GLSurfaceView实例
         glSurfaceView = GLSurfaceView(this)
+        //检测当前设备是否支持OpenGLES2.0
         val isSupportES2 = supportES2(applicationContext)
         if (isSupportES2) {
+            //设置要使用的EGLContext client version
             glSurfaceView.setEGLContextClientVersion(2)
+            //设置渲染器
             glSurfaceView.setRenderer(OpenGlRender())
             rendererSet = true
         } else {
-            Toast.makeText(applicationContext, "This device does not support OpenGL ES 2.0 !", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                applicationContext,
+                "This device does not support OpenGL ES 2.0 !",
+                Toast.LENGTH_SHORT
+            ).show()
             return
         }
         setContentView(glSurfaceView)
