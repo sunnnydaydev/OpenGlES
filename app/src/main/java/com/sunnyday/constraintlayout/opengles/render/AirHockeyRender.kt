730f9a750d0575d6b464c99b83313dfae77f44c6
SunnyDay <1246771571@qq.com>
2022-04-04 11:08:37 +0800

    1、编译着色器&在屏幕上绘图finish 2、留个bug

--- app/src/main/java/com/sunnyday/constraintlayout/opengles/render/AirHockeyRender.kt
+++ app/src/main/java/com/sunnyday/constraintlayout/opengles/render/AirHockeyRender.kt
@@ -9,6 +9,7 @@ import com.sunnyday.constraintlayout.opengles.utils.LoggerConfig
 import com.sunnyday.constraintlayout.opengles.utils.TextResourceReader
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
+import java.nio.FloatBuffer
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -16,12 +17,38 @@ import javax.microedition.khronos.opengles.GL10
  * Create by SunnyDay on 20:26 2021/12/26
  */
 class AirHockeyRender(private val mContext: Context) : GLSurfaceView.Renderer {
+    // 矩形就是两个三角形，注意逆时针顺序定义。
+    private val tableVerticesWithTriangle = floatArrayOf(
+        //第一个三角形
+        -0.5f, -0.5f,
+        0.5f, 0.5f,
+        -0.5f, 0.5f,
+        // 第二个三角形
+        -0.5f, -0.5f,
+        0.5f, -0.5f,
+        0.5f, 0.5f,
+        // 中间的分割线
+        -0.5f, 0f,
+        0.5f, 0f,
+        // 两木追顶点
+        0f, -0.25f,
+        0f, 0.25f
+    )
+
+    //定义分配本地堆数据，这样着色器就可以读取本地的数据了。
+    private var vertexData = ByteBuffer
+        .allocateDirect(tableVerticesWithTriangle.size * BYTE_PER_FLOAT)
+        .order(ByteOrder.nativeOrder())
+        .asFloatBuffer()
+        .put(tableVerticesWithTriangle) //吧内存从java堆赋值到本地堆
+
     private var program = 0
     private var uColorLocation: Int = 0
     private var aPositionLocation: Int = 0
 
     companion object {
         private const val POSITION_COMPONENT_COUNT = 2
+
         // 每个float变量所占字节数
         private const val BYTE_PER_FLOAT = 4
         private const val U_COLOR = "u_Color"
@@ -36,31 +63,6 @@ class AirHockeyRender(private val mContext: Context) : GLSurfaceView.Renderer {
             9f, 14f,
             9f, 0f
         )
-        // 矩形就是两个三角形，注意逆时针顺序定义。
-        val tableVerticesWithTriangle = floatArrayOf(
-            //第一个三角形
-            0f, 0f,
-            9f, 14f,
-            0f, 14f,
-            // 第二个三角形
-            0f, 0f,
-            9f, 0f,
-            9f, 14f,
-            // 中间的分割线
-            0f, 7f,
-            9f, 7f,
-            // 两木追顶点
-            4.5f, 2f,
-            4.5f, 12f
-        )
-
-        //定义分配本地堆数据，这样着色器就可以读取本地的数据了。
-        val vertexData = ByteBuffer
-            .allocateDirect(tableVerticesWithTriangle.size * BYTE_PER_FLOAT)
-            .order(ByteOrder.nativeOrder())
-            .asFloatBuffer()
-            .put(tableVerticesWithTriangle) //吧内存从java堆赋值到本地堆
-
     }
 
     /**
@@ -72,6 +74,15 @@ class AirHockeyRender(private val mContext: Context) : GLSurfaceView.Renderer {
     override fun onDrawFrame(gl: GL10?) {
         // 清空屏幕所有颜色，
         glClear(GL_COLOR_BUFFER_BIT)
+        // 绘制桌子
+        glUniform4f(uColorLocation, 1f, 1f, 1f, 1f)
+        glDrawArrays(GL_TRIANGLES, 0, 6)
+        //绘制分割线
+        glUniform4f(uColorLocation, 1f, 0f, 0f, 1f)
+        glDrawArrays(GL_LINES, 6, 2)
+        //木追绘制为点
+        glUniform4f(uColorLocation, 0f, 0f, 1f, 1f)
+        glDrawArrays(GL_POINTS, 9, 1)
     }
 
     /**
@@ -83,15 +94,6 @@ class AirHockeyRender(private val mContext: Context) : GLSurfaceView.Renderer {
          * 设置视口（Viewport）尺寸，也即告诉OpenGL渲染view的大小。
          * */
         glViewport(0, 0, width, height)
-        // 验证程序
-        if (LoggerConfig.isOpenLogger) {
-            ShaderHelper.validateProgram(program)
-        }
-        // 使用程序
-        glUseProgram(program)
-        //获取uniform位置
-        uColorLocation = glGetUniformLocation(program, U_COLOR)
-        aPositionLocation = glGetAttribLocation(program, A_POSITION)
     }
 
     /**
@@ -102,9 +104,9 @@ class AirHockeyRender(private val mContext: Context) : GLSurfaceView.Renderer {
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
         /**
          * 设置清空屏幕用的颜色，参数：red、green、blue、alpha
-         * [0,1]代表颜色强度。如下屏幕清空会显示红色。
+         * [0,1]代表颜色强度。如下屏幕清空会显示黑色。
          * */
-        glClearColor(1f, 0f, 0f, 0f)
+        glClearColor(0f, 0f, 0f, 0f)
         // 读取、编译、获取着色器
         val vertexShaderSource =
             TextResourceReader.readTextFileFromResource(mContext, R.raw.simple_vertex_shader)
@@ -113,5 +115,28 @@ class AirHockeyRender(private val mContext: Context) : GLSurfaceView.Renderer {
         val vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource)
         val fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource)
         program = ShaderHelper.linkProgram(vertexShader, fragmentShader)
+
+        // 验证程序
+        if (LoggerConfig.isOpenLogger) {
+            ShaderHelper.validateProgram(program)
+        }
+        // 使用程序
+        glUseProgram(program)
+        //获取uniform位置
+        uColorLocation = glGetUniformLocation(program, U_COLOR)
+        aPositionLocation = glGetAttribLocation(program, A_POSITION)
+
+        //关联属性与顶点数据数组
+        vertexData.position(0)
+        glVertexAttribPointer(
+            aPositionLocation,
+            POSITION_COMPONENT_COUNT,
+            GL_FLOAT,
+            false,
+            0,
+            vertexData
+        )
+        //使能够顶点数组
+        glEnableVertexAttribArray(aPositionLocation)
     }
 }
--- app/src/main/res/raw/simple_vertex_shader.glsl
+++ app/src/main/res/raw/simple_vertex_shader.glsl
@@ -4,4 +4,6 @@
 attribute vec4 a_Position;
 void main(){
     gl_Position = a_Position;
+    // 指定点的大小
+    glz_PointSize =10.0;
 }
